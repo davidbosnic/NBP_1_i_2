94e6e288524130a2fae67a347d710fe2118e30f8
begota98 <begota98@gmail.com>
2021-01-04 22:52:31 +0100

    sveee

--- My_Face/Pages/Index.cshtml.cs
+++ My_Face/Pages/Index.cshtml.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Neo4j.Driver;
+using Neo4j.Driver.V1;
+using Neo4jClient;
+using Neo4jClient.Cypher;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +15,28 @@ namespace My_Face.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private BoltGraphClient client;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            IDriver driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "1234"), Config.Builder.WithEncryptionLevel(EncryptionLevel.None).ToConfig());
 
+
+            client = new BoltGraphClient(driver: driver);
+            
+            try
+            {
+                client.Connect();
+            }
+            catch (Exception exc)
+            {
+                //MessageBox.Show(exc.Message);
+            }
         }
     }
 }
